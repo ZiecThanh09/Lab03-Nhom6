506d93845ccb5b41596cdee613ddab9ee7cc9657
Anh Đức <104743721+ADUK2@users.noreply.github.com>
2022-05-04 10:22:15 +0700

    Add files via upload

---
+++ Server.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Lab03_Bai02
+{
+    partial class Server
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartListen = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // StartListen
+            // 
+            this.StartListen.Location = new System.Drawing.Point(447, 22);
+            this.StartListen.Name = "StartListen";
+            this.StartListen.Size = new System.Drawing.Size(104, 39);
+            this.StartListen.TabIndex = 0;
+            this.StartListen.Text = "Listen";
+            this.StartListen.UseVisualStyleBackColor = true;
+            this.StartListen.Click += new System.EventHandler(this.StartListen_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 67);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(539, 374);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // Server
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(563, 453);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.StartListen);
+            this.Name = "Server";
+            this.Text = "Server";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartListen;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ Server.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Lab03_Bai02
+{
+    public partial class Server : Form
+    {
+        public Server()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void StartListen_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Connecting...");
+            StartListen.Enabled = false;
+            CheckForIllegalCrossThreadCalls = false;
+            Thread serverThread = new Thread(new ThreadStart(StartUnsafeThread));
+            serverThread.Start();            
+        }
+
+        void StartUnsafeThread()
+        {
+            int bytesReceived = 0;
+            byte [] recv = new byte[1];
+            Socket clientSocket;
+            Socket listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ipepServer = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+            listenerSocket.Bind(ipepServer);
+            listenerSocket.Listen(-1);
+            clientSocket = listenerSocket.Accept();
+            listView1.Items.Add(new ListViewItem("New client Connected."));
+            while(clientSocket.Connected)
+            {
+                string text = "";
+                do
+                {
+                    bytesReceived = clientSocket.Receive(recv);
+                    text += Encoding.ASCII.GetString(recv);
+                } while (text[text.Length - 1] != '\n');
+                listView1.Items.Add(new ListViewItem(text));
+            }            
+            clientSocket.Close();
+        }
+
+    }
+}
